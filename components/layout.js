2cef54d5d543ef74a9b08f510e79df1c004d6437
matthewlazewski <matthew.lazewski@gmail.com>
2021-01-14 13:43:49 -0600

    finished layout.js

--- components/layout.js
+++ components/layout.js
@@ -21,7 +21,46 @@ export default function Layout({ children, home }) {
                         siteTitle
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content ="summarry_large_image" />
             </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <img
+                            src="/images/profile.jpg"
+                            className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                            alt={name}
+                        />
+                        <h1 className={utilStyles.heading2xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <a>
+                                <img
+                                    src="/images/profile.jpg"
+                                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                                    alt={name}
+                                />
+                            </a>
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{name}</a>
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a><- Back to home</a>
+                    </Link>
+                </div>
+            )}
         </div>
     )
 }
