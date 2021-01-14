ca860a923ff5d5d1a33b7cbdb0b4d9b041d70932
matthewlazewski <matthew.lazewski@gmail.com>
2021-01-14 11:44:08 -0600

    updated layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,27 @@
+import Head from 'next/head'
 import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>
+const name = 'Matt Lazewski'
+export const siteTitle = 'Next.js Blog Playground'
+
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="description"
+                    content="Building a blog site with Next.js"
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.now.sh/${encodeURI(
+                        siteTitle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+            </Head>
+        </div>
+    )
 }
